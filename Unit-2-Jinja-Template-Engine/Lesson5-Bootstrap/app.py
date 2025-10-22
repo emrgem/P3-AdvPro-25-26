48d053e9d76a9e72a2777459b4eaf241ecf09436
emrgem <emrgem@bergen.org>
2025-10-22 11:07:28 -0400

    bootstrap simple page completed

--- Unit-2-Jinja-Template-Engine/Lesson5-Bootstrap/app.py
+++ Unit-2-Jinja-Template-Engine/Lesson5-Bootstrap/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return render_template('home.html')
 
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True, port=5050)
