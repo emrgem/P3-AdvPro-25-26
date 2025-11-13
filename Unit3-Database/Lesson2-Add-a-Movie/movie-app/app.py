b3710390a25c64dc024f58b38d207953064ea1a8
emrgem <emrgem@bergen.org>
2025-11-13 10:34:09 -0500

    add movie route get method, inital view is set

--- Unit3-Database/Lesson2-Add-a-Movie/movie-app/app.py
+++ Unit3-Database/Lesson2-Add-a-Movie/movie-app/app.py
@@ -119,6 +119,15 @@ def about():
     """About CineMatch page"""
     return render_template('about.html')
 
+@app.route('/add_movie', methods=['GET', 'POST'])
+def add_movie():
+    """Add a new movie to the database"""
+    if request.method == 'POST':
+        #We will add this next
+        pass
+    
+    #If GET request, just show the form
+    return render_template("add_movie.html")
 
 # ============================================================================
 # ERROR HANDLERS
